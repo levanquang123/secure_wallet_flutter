dc9fdf716dece9c4cfc66082c0076ef2eac8b2a4
levanquang123 <levanquang27122005@gmail.com>
2026-01-19 18:24:23 +0700

    feat(auth): define login states

---
+++ lib/presentation/auth/login/login_state.dart
@@ -0,0 +1,21 @@
+abstract class LoginState {
+  const LoginState();
+}
+
+class LoginInitial extends LoginState {
+  const LoginInitial();
+}
+
+class LoginLoading extends LoginState {
+  const LoginLoading();
+}
+
+class LoginSuccess extends LoginState {
+  const LoginSuccess();
+}
+
+class LoginError extends LoginState {
+  final String message;
+
+  const LoginError(this.message);
+}
