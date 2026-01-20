30107c0555418e7306c7cc7f342e85709470708e
levanquang123 <levanquang27122005@gmail.com>
2026-01-20 13:29:51 +0700

    feat(wallet): add get wallet use case

--- lib/domain/wallet/get_wallet_useCase.dart
+++ lib/domain/wallet/get_wallet_useCase.dart
@@ -1,3 +1,12 @@
-class GetWalletUsecase {
+import 'wallet_repository.dart';
 
+class GetWalletUseCase {
+  final WalletRepository walletRepository;
+
+  GetWalletUseCase(this.walletRepository);
+
+  Future<double> getBalance() async {
+
+    return await walletRepository.getBalance();
+  }
 }
--- lib/presentation/wallet/wallet_view_model.dart
+++ lib/presentation/wallet/wallet_view_model.dart
@@ -1,3 +1,28 @@
-class WalletViewModel {
+import 'package:flutter/cupertino.dart';
+import 'package:secure_wallet_flutter/domain/wallet/get_wallet_useCase.dart';
+import 'package:secure_wallet_flutter/presentation/wallet/wallet_state.dart';
 
+class WalletViewModel extends ChangeNotifier {
+  final GetWalletUseCase getWalletUseCase;
+
+  WalletViewModel(this.getWalletUseCase);
+
+  late WalletState _state = const WalletInitial();
+
+  WalletState get state => _state;
+
+  void _setState(WalletState newState) {
+    _state = newState;
+    notifyListeners();
+  }
+
+  Future<void> loadWallet() async {
+    _setState(const WalletLoading());
+    try {
+      final balance = await getWalletUseCase.getBalance();
+      _setState(WalletLoaded(balance));
+    } catch (e) {
+      _setState(WalletError(e.toString()));
+    }
+  }
 }
